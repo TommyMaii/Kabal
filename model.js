d661642d2bb0d82fdce208ef2dcd3a17b06c50f2
Tommy Mai <tommymaimai@gmail.com>
2024-09-17 13:25:28 +0200

    deck now works

--- model.js
+++ model.js
@@ -1,12 +1,12 @@
 let currentCard;
 let swappedCard;
 let playedCards = [];
-let randomType;
 let firstTime = true;
 const cardSuit = {0: "Spades", 1: "Clubs", 2: "Diamonds", 3:"Hearts"}
 const cardValue = {12:'King', 11:'Queen', 10:'Jack', 9:'10', 8:'9', 7:'8', 6:'7', 5:'6', 4:'5', 3:'4', 2:'3', 1:'2', 0:'Ace'}
 const cardBack = 'cardbackred'
-let allCardsList = [];
+let allCardsList = []
+let runs = 28;
 
 
 //Tested different methods
@@ -21,4 +21,15 @@ let cardStructure = {
     // ],
 }
 
-let deckStructure = {}
+let deckStructure = {
+//     [{
+//             cardSuit: "Clubs2",
+//             cardValue: 'Jack',
+//             shown: true,
+//    }]
+//     [{
+//             cardSuit: "Clubs2",
+//             cardValue: 'Jack',
+//             shown: true,
+//    }]
+}
