3d8ee35f92590beb5efa1cde099e43004c246e68
Tommy Mai <tommymaimai@gmail.com>
2024-09-18 15:03:53 +0200

    x

--- model.js
+++ model.js
@@ -1,18 +1,18 @@
-let currentGeneratedCard;
-let playedCards = [];
+let currentGeneratedCard = '';
+const playedCards = [];
 let firstTime = true;
 const cardSuit = {0: "Spades", 1: "Clubs", 2: "Diamonds", 3:"Hearts"}
 const cardValue = {12:'King', 11:'Queen', 10:'Jack', 9:'10', 8:'9', 7:'8', 6:'7', 5:'6', 4:'5', 3:'4', 2:'3', 1:'2', 0:'Ace'}
 const cardBack = 'cardbackred'
-let allCardsList = []
+const allCardsList = []
 let runs = 28;
+let colors = {0: 'black', 1: 'black', 2: 'red', 3: 'red'};
 
 
 //Tested different methods
 let cardStructure = {
     currentCard : {
-        row: "row2",
-        index: 1
+
     },
     swappedCard : {
         row: "row1",
@@ -21,8 +21,8 @@ let cardStructure = {
     // Example Structure
     // "row1": [
         // {
-        //     cardSuit: "Clubs2",
-        //     cardValue: 'Jack',
+        //     cardSuits: "Clubs2",
+        //     cardValues: 'Jack',
         //     shown: true,
         // },
     // ],
@@ -30,15 +30,24 @@ let cardStructure = {
 
 let deckStructure = {
 //     [{
-//             cardSuit: "Clubs2",
-//             cardValue: 'Jack',
+//             cardSuits: "Clubs2",
+//             cardValues: 'Jack',
 //             shown: true,
 //    }]
 //     [{
-//             cardSuit: "Clubs2",
-//             cardValue: 'Jack',
+//             cardSuits: "Clubs2",
+//             cardValues: 'Jack',
 //             shown: true,
 //    }]
+}
+
+let firstDeck = {
+//     [{
+//             cardSuits: "Clubs2",
+//             cardValues: 'Jack',
+//             shown: true,
+//    }]
+
 }
 
 
